c554fcba380c140b3d37b6bb9fd44b50d9e1d957
paulo <paulo@DESKTOP-E3MG5IE>
2023-04-12 10:12:40 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            HomeModel home = new();
+
+            home.Nome = "Paulo Fabricio";
+            home.Email = "paulofsm95@gmail.com";
+            home.Desenvolvedor = "Sou o Mais novo Desenvolvedor C#";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/HomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC.Models
+{
+    public class HomeModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Desenvolvedor { get; set; }
+    }
+}
